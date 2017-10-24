e9880ec6e0c8e2ad858c0fe3d39d0fcd0a302708
IvanDarioDavid <ivandario.david@estudiants.urv.cat>
2017-10-24 17:11:30 +0200

    Clean code

--- app/src/main/java/cat/urv/deim/android/service/ThirdActivity.java
+++ app/src/main/java/cat/urv/deim/android/service/ThirdActivity.java
@@ -16,28 +16,28 @@ import android.widget.TextView;
 
 //Definicion dinamica. La diferencia es que aqui se define cuando se va a usar mientras que en la estatica se usa siempre
 //Definir el uso de un BroadcastReciver registrado
-//Se debe hacer el registro (en el manifest) dinamicamente, sino que cuando se arranque la activity tiene que hacerse el registro
+//No Se debe hacer el registro en el manifest dinamicamente, sino que cuando se arranque la activity tiene que hacerse el registro
 //Cuando pare la activity tiene que desenregistrarse
 
 
 public class ThirdActivity extends AppCompatActivity {
 
-    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String resultado = intent.getExtras().getString(MyIntentService.KEY_RESULTADO);
-            if (text != null) {
-                text.setText(resultado);
-                Intent intent_send_service = new Intent(context, MyIntentService.class);
-                intent_send_service.putExtra(  MyIntentService.KEY_ACTION, MyIntentService.KEY_STOP);
-                context.startService(intent_send_service);//ejecuta onHandleIntent
+        BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String resultado = intent.getExtras().getString(MyIntentService.KEY_RESULTADO);
+                if (text != null) {
+                    text.setText(resultado);
+                    Intent intent_send_service = new Intent(context, MyIntentService.class);
+                    intent_send_service.putExtra(  MyIntentService.KEY_ACTION, MyIntentService.KEY_STOP);
+                    context.startService(intent_send_service);//ejecuta onHandleIntent
+                }
+                else
+                    Log.e(TAG,"Encara no s'ha creat l'activity");
             }
-            else
-                Log.e(TAG,"Encara no s'ha creat l'activity");
-        }
-    };
+        };
 
-    private Button send_button;
+    //private Button send_button;
     private static TextView text;
     protected static final String TAG = SecondActivity.class.getSimpleName();
 
@@ -49,22 +49,7 @@ public class ThirdActivity extends AppCompatActivity {
 
         text = (TextView)findViewById(R.id.textView3);
 
-        registerReceiver(broadcastReceiver, new IntentFilter("AppService"));
+        registerReceiver(broadcastReceiver, new IntentFilter("AppService")); //El recibidor puede recibir broadcasts de diferentes Apps.
     }
-
-    /*public static class NumberBroadcastReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String resultado = intent.getExtras().getString(MyIntentService.KEY_RESULTADO);
-            if (text != null) {
-                text.setText(resultado);
-                Intent intent_send_service = new Intent(context, MyIntentService.class);
-                intent_send_service.putExtra(  MyIntentService.KEY_ACTION, MyIntentService.KEY_STOP);
-                context.startService(intent_send_service);//ejecuta onHandleIntent
-            }
-            else
-                Log.e(TAG,"Encara no s'ha creat l'activity");
-        }
-    }*/
 
 }
